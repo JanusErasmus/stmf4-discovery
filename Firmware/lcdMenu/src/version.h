e392dac1e111b12d0efc8efff59dd7ead94a4b84
Janus Erasmus <jcerasmus@gmail.com>
2015-03-23 14:39:34 +0200

    data byte pins type can be configured

--- Firmware/lcdMenu/src/version.h
+++ Firmware/lcdMenu/src/version.h
@@ -1,5 +1,5 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 #define VERSION_NUM 0x000001 
-#define BUILD_DATE "Mon Mar 23 14:22:25 SAST 2015" 
+#define BUILD_DATE "Mon Mar 23 14:37:23 SAST 2015" 
 #endif 
